a0e42a0373c1a03d11902a742531a464ca4b8692
srielango <srielango@gmail.com>
2024-10-16 17:01:15 +0530

    Added clean architecture projects

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TodoList> TodoLists => Set<TodoList>();
+
+        public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+
+    }
+}
